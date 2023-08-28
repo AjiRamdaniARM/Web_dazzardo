09305372de4cad7bbd400a408809f2ba301c7579
AjiRamdaniARM <hilmankeren0936@gmail.com>
2023-08-28 18:26:48 +0700

    ari-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let express = require ('express');
+let app = express();
+let path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.render('pages/home');
+})
+app.get('/Events', (req,res) => {
+    res.render('pages/Events')
+})
+
+
+app.use(express.static(path.join(__dirname,'views')));
+
+app.listen(3000);
